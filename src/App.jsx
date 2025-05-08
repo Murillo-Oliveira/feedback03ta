ed8607ef24b8fa9daa7237b2680294ca0f6ce8dc
murilloscoliveira <murilloscoliveira@gmail.com>
2025-05-08 20:40:44 +0000

    criação do arquivo cardfinal para receber as informações do card final

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
+import { CardFinal } from "./components/CardFinal"
 import { CardInicial } from "./components/CardInicial"
-import illustrationThankYou from "./assets/illustration-thank-you.svg"
 import { useState } from "react"
 
 export function App() {
@@ -16,15 +16,7 @@ return (
   submited === false ? (
    <CardInicial setNota={setNota} setSubmited={setSubmited} nota={nota}/>
   ) : (
-    <div className="max-w-103 bg-gradient-dark p-6 rounded-2xl text-white font-overpass text-center">
-      <img className="mx-auto mb-6" src={illustrationThankYou} alt="" />
-
-      <p className="mb-6 text-orange bg-dark-blue w-fit mx-auto px-3 py-1.25 rounded-3xl text-sm">You selected {nota} out of 5</p>
-
-      <h1 className="text-2xl font-bold mb-2.5">Thank You!</h1>
-
-      <p className="text-sm text-light-grey mb-6">We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!</p>
-    </div>
+    <CardFinal nota={nota}/>
   )
   )
 }
